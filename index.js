eaa493cd48a004d476be962c72061b6df660a49c
Fabien LOISON <flozz@users.noreply.github.com>
2018-08-10 17:44:04 +0200

    Fixes issue when the @class name is missing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ exports.handlers = {
         var classMatch = e.source.match(/\/\*\*[\s\S]*@class\s+(.*?)\s*$[\s\S]*?\*\//m);
         var className = classMatch ? classMatch[1] : null;
 
-        if (className) {
+        if (className && className.trim().length) {
             e.source = e.source.replace(/(.*?\s?=\s?.*?\.\$extend\(\X*?)(\{)/, "$1/** @lends " + className + " */$2");
             e.source = e.source.replace(/@(member|event|fires|emits)\s+((\{.*\})\s+)?(.*?)\s*$/gm, "@$1 $3" + className + "#$4");
         }
